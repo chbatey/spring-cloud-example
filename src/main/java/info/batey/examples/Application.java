8c398efa5c42d3cc8d9952e963098cec7c28accf
chbatey <christopher.batey@gmail.com>
2014-10-23 09:14:43 +0100

    adding a hystrix dashboard

--- src/main/java/info/batey/examples/Application.java
+++ src/main/java/info/batey/examples/Application.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @EnableHystrix
 @RestController
+@EnableHystrixDashboard
 public class Application {
 
     @Autowired
